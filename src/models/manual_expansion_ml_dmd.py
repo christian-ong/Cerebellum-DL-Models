cc1164280bf0843d1544576a98c28b2dc7d74256
christian-ong <s204109@dtu.dk>
2026-03-03 15:48:51 +1100

    expanded basis-expansion: bias term and 1st order cos/sin. Also fixed order of terms

--- src/models/manual_expansion_ml_dmd.py
+++ src/models/manual_expansion_ml_dmd.py
@@ -13,20 +13,44 @@ class ManualExpansion_MLDMD(nn.Module):
         -> x_next (de-expanded to original space)
     """
 
-    def __init__(self, state_dim=2, expansion_degree=3):
+    def __init__(
+            self, 
+            state_dim=2, 
+            expansion_degree=3, 
+            constant_expansion=True, 
+            sine_cosine_expansion=True
+        ):
+
         super().__init__()
 
-        # Add basis expansion of the state
-        self.polynomial_expansions = []
+        self.expanded_basis = []
         self.expand_names = []
+
+        # Constant term
+        if constant_expansion:
+            self.expanded_basis = [(0, 0)]
+            self.expand_names = ["1"]
+
+        # Polynomial expansion of the state 
         for d in range(1, expansion_degree + 1):
             for i in range(d + 1):
                 e_x = d-i
                 e_y = i
-                self.polynomial_expansions.append((e_x, e_y))
-                self.expand_names.append(f"x^{e_x} y^{e_y}")
-        self.expanded_dim = len(self.polynomial_expansions)
-        
+                self.expanded_basis.append((e_x, e_y))
+
+                expansion_string = f"x^{e_x} y^{e_y}".replace("x^0", "").replace("y^0", "").replace("^1", "").strip()
+                self.expand_names.append(expansion_string)
+        self.expanded_dim = len(self.expanded_basis)
+
+        # Sine and cosine expansion of the state
+        if sine_cosine_expansion:
+            for var in ["x", "y"]:
+                for func in ["sin", "cos"]:
+                    self.expanded_basis.append((func, var))
+                    self.expand_names.append(f"{func}({var})")
+            self.expanded_dim += 4
+
+        # Linear dynamics in the expanded space
         self.K = nn.Linear(
             in_features=self.expanded_dim,
             out_features=self.expanded_dim,
@@ -37,8 +61,18 @@ class ManualExpansion_MLDMD(nn.Module):
     def expand(self, x):
         expanded_features = []
 
-        for i, j in self.polynomial_expansions:
-            expanded_features.append((x[:, 0] ** i) * (x[:, 1] ** j))
+        for i, j in self.expanded_basis:
+            if isinstance(i, int) and isinstance(j, int):
+                expanded_features.append((x[:, 0] ** i) * (x[:, 1] ** j))
+            elif isinstance(i, str) and isinstance(j, str):
+                if i == "sin":
+                    expanded_features.append(torch.sin(x[:, 0]))
+                elif i == "cos":
+                    expanded_features.append(torch.cos(x[:, 0]))
+                if j == "sin":
+                    expanded_features.append(torch.sin(x[:, 1]))
+                elif j == "cos":
+                    expanded_features.append(torch.cos(x[:, 1]))
 
         x_expanded = torch.stack(expanded_features, dim=1)
 
