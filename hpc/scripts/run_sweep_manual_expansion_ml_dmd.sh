040cc31934569b39f6d1d6494f9d36a63d0eaf83
danielries <s214641@dtu.dk>
2026-03-18 20:29:39 +1100

    adding some hpc stuff

---
+++ hpc/scripts/run_sweep_manual_expansion_ml_dmd.sh
@@ -0,0 +1,41 @@
+# LSBATCH: User input
+#!/bin/bash
+#BSUB -J koopman_manual_exp_ml
+
+# Output files
+#BSUB -o runs/%J.out
+#BSUB -e runs/%J.err
+
+# GPU
+#BSUB -q gpua10
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "rusage[mem=16GB] span[hosts=1]"
+#BSUB -n 4
+
+# Runtime
+#BSUB -W 04:00
+
+# ----------------------------------
+# ENV
+# ----------------------------------
+
+cd /dtu/blackhole/0d/168141/Cerebellum-DL-Models
+
+module load python3/3.11.11
+source .venv/bin/activate
+
+export WANDB_START_METHOD=thread
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+# ----------------------------------
+# SWEEP
+# ----------------------------------
+
+SWEEP_ID=DeepLearningP4Destruction/Cerebellum-DL-Models/<SWEEP_ID>
+
+# ----------------------------------
+# RUN (ONLY ONE AGENT!)
+# ----------------------------------
+
+wandb agent $SWEEP_ID
