6d2af6e0380f50fe2d5686d94149d7eb2cc8fd8a
Daniel Ries <s214641@dtu.dk>
2026-06-11 10:02:38 +0200

    simulate fix

--- scripts/simulate_data.py
+++ scripts/simulate_data.py
@@ -71,11 +71,11 @@ System-specific parameters:
 Linear system  x' = A x
 --------------------------------------------------
 # dt = 0.01
-python -m scripts.simulate_data --system inward_spiral --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system inward_spiral_cw --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system harmonic_oscillator --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system saddle_point --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system degenerate_node --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
+python -m scripts.simulate_data --system inward_spiral --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system inward_spiral_cw --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system harmonic_oscillator --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system saddle_point --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system degenerate_node --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
 
 # dt = 0.05
 python -m scripts.simulate_data --system inward_spiral --T_train 15.0 --n_traj_train 400 --T_val 15.0 --n_traj_val 40 --T_test 15.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.05 --name dt_0.05
@@ -88,16 +88,16 @@ python -m scripts.simulate_data --system degenerate_node --T_train 15.0 --n_traj
 Nonlinear systems
 --------------------------------------------------
 # dt = 0.01
-python -m scripts.simulate_data --system closed_small --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system closed_large --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system closed_trig_small --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system closed_trig_medium --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system closed_trig_large --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system vanderpol --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system lotka_volterra --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system pendulum --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system duffing --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
-python -m scripts.simulate_data --system lorenz --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01 --debug phase_portrait
+python -m scripts.simulate_data --system closed_small --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system closed_large --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system closed_trig_small --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system closed_trig_medium --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system closed_trig_large --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system vanderpol --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system lotka_volterra --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system pendulum --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system duffing --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
+python -m scripts.simulate_data --system lorenz --T_train 3.0 --n_traj_train 400 --T_val 3.0 --n_traj_val 40 --T_test 3.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.01 --name dt_0.01
 
 # dt = 0.05
 python -m scripts.simulate_data --system closed_small --T_train 15.0 --n_traj_train 400 --T_val 15.0 --n_traj_val 40 --T_test 15.0 --n_traj_test 40 --max_rollout_horizon 100 --dt 0.05 --name dt_0.05
